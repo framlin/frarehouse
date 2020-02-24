abc2a1f8448639cb023e3fb0a40cccc8a5e5157f
Wolfgang Egger <w.egger@framlin.com>
2020-02-24 23:04:57 +0100

    implement create and put

--- src/console/Interpreter.py
+++ src/console/Interpreter.py
@@ -1,17 +1,42 @@
 import Token
+import os
+from pathlib import Path
+import subprocess
+import shutil
 
 class Cmd:
     quitConsole = False
     userInput = ""
+    fundusPath = "../../data/fundus"
+    stockPath = "../../data/stock"
+    dataPath = "../../data"
 
     def execute_put(self):
-        print("PUT")
+        item = input("Item: ")
+        container = input("Container: ")
+        print("put Item " + item + " into " + container)
+        itemPath = subprocess.run(['find', self.dataPath, '-name', item], capture_output=True).stdout.decode('utf-8').rstrip()
+        containerPath = subprocess.run(['find', self.dataPath, '-name', container], capture_output=True).stdout.decode('utf-8').rstrip()
+        shutil.move(itemPath, containerPath)
+        print(os.listdir(containerPath))
 
     def execute_create_container(self):
         print("CREATE_CONTAINER")
+        container = input('Container: ')
+        container_path = self.stockPath + "/" + container
+
+        if not os.path.exists(container_path):
+            os.makedirs(container_path)
+            print("Container " + container_path + " created")
 
     def execute_create_Item(self):
         print("CREATE_ITEM")
+        item = input('Item: ')
+        item_path = self.stockPath + "/" + item
+
+        Path(item_path).touch()
+        print("Item " + item_path + " created")
+
 
     def execute_show(self):
         print("SHOW")
